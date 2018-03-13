b5696b746d052b6d8f71ff3ebc4f48462b655d66
FabAstorga06 <astorgafabian6@gmail.com>
2018-03-13 08:51:14 -0600

    Agrega algunos algoritmos de encriptacion de imagenes en escala de grises.

--- Proyectos/Proyecto 1: Procesador Vectorial/proc_img/image_proc.c
+++ Proyectos/Proyecto 1: Procesador Vectorial/proc_img/image_proc.c
@@ -1,18 +1,15 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-#include <cv.h>
-#include <highgui.h>
+#include "encrypt_algs.c"
 
 #define SIZE_W 100
 #define TITLE_W "Convertidor Imagenes"
 
 /*********************************************************************************/
 
+int img_h, img_w, img_channels;
+
 IplImage* convert2gray(char* img_name ) {
 
   IplImage* img_proc = 0;
-  int img_h, img_w, img_channels;
   // Carga una imagen
   img_proc = cvLoadImage(img_name, CV_LOAD_IMAGE_COLOR );
   // Verifica si la imagen se cargo exitosamente
@@ -20,17 +17,16 @@ IplImage* convert2gray(char* img_name ) {
     printf("No fue posible cargar la imagen: %s\n",img_name);
     exit(0);
   }
-  // Obtiene los datos de la imagen **
-  img_h = img_proc->height;
-  img_w = img_proc->width;
-  img_channels = img_proc->nChannels;
-  printf("Procesando imagen de %dx%d con %d canales.\n",img_h,
-                                                      img_w,img_channels);
+
+  int img_channels = img_proc->nChannels;
+  printf("Procesando imagen de %dx%d con %d canales.\n",img_proc->height,
+                                            img_proc->width,img_channels);
   // Crea ventana para mostrar imagen
   cvNamedWindow(TITLE_W, CV_WINDOW_AUTOSIZE);
   cvMoveWindow(TITLE_W, SIZE_W, SIZE_W);
   // Se crea un objeto que será la nueva imagen gris
-  IplImage* img_gray = cvCreateImage(cvSize(img_w, img_h), IPL_DEPTH_8U, 1);
+  IplImage* img_gray = cvCreateImage(cvSize(img_proc->width, img_proc->height),
+                                            IPL_DEPTH_8U, 1);
   // Cambia escala de imagen de RGB a gris
   cvCvtColor(img_proc, img_gray, CV_RGB2GRAY);
   return img_gray;
@@ -41,7 +37,6 @@ void show_image (IplImage* img) {
   cvShowImage(TITLE_W, img );
   // Espera por una señal del teclado para cerrar ventana
   cvWaitKey(0);
-  cvReleaseImage(&img );
 }
 
 /*********************************************************************************/
@@ -54,7 +49,19 @@ int main(int argc, char *argv[] )  {
     exit(0);
   }
 
-  show_image( convert2gray(argv[1]) );
+  /* Se convierte imagen a escala de grises */
+  IplImage* img_g = convert2gray(argv[1] );
+  show_image(img_g);
+
+  /* Se cambia de imagen a matriz */
+  CvMat* mtx_g = cvCreateMat(img_g->height, img_g->width, CV_8UC1 );
+  cvConvert( img_g, mtx_g );
+
+  /* Encriptacion */
+  //xor_encrypt(mtx_g, img_g->height, img_g->width);
+  //simple_desp(mtx_g, img_g->height, img_g->width);
+  // circular_desp(mtx_g, img_g->height, img_g->width);
+  simple_add(mtx_g, img_g->height, img_g->width);
 
   return 0;
 }
